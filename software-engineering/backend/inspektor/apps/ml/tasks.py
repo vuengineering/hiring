2b1b06fd6e7bbaa61f1402fbcf92db8d3bf0141d
Angel Blanco <angel.blanco@planhat.com>
2024-02-16 20:35:09 +0100

    INSPEKTOR ML + Mocked version of run_inference_on_image to run Image inspections.

--- software-engineering/backend/inspektor/apps/ml/tasks.py
+++ software-engineering/backend/inspektor/apps/ml/tasks.py
@@ -1,13 +1,32 @@
-from inspektor.apps.core.models import Image
+from random import randint
+from inspektor.apps.core.models import Image, Inspection
+
+Errors = {
+    "ERROR_001": "This is inspection error 1 happening...",
+    "ERROR_002": "This is inspection error 2 happening...",
+    "ERROR_003": "This is inspection error 3 happening...",
+    "ERROR_004": "This is inspection error 4 happening...",
+}
 
 
 def run_inference_on_image(image: Image):
     """
-    Implement this function to run inference on the given image.
+    This is a mocked version of the inspection algorithm.
+    TODO: Possible bottleneck here. Think about queueing inspections using Google PubSub or similar.
+
+
     Args:
         image: Image
 
-    Returns: ???
+    Returns: Inspection model instance after saving into DB.
 
     """
-    pass
+
+    result = randint(0, 1)
+    inspection = Inspection(
+        inspection_result=result,
+        image=image,
+        inspection_errors=[] if result else [Errors["ERROR_001"], Errors["ERROR_003"]],
+    )
+    inspection.save()
+    return inspection
